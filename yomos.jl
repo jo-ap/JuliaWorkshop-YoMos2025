fc8bd89980435115a0f61d38f0082910efe80aea
jo-ap <johannes.apelt@posteo.de>
2025-05-14 15:29:40 +0200

    worked on workshop

--- yomos.jl
+++ yomos.jl
@@ -1,17 +1,95 @@
-## Multiple Dispatch: A key feature of julia 
+## [1] Warm up 
+## [1.1] Some basic collection types 
 
+# constructor
+x = [1, 2, 3] # Vector{Int64}
+y = [1.0, 2, 3] # Vector{Float64}
+nt = (a=1.9, b=2, c="hello world") # named tuple (immutable), has no fixed element type
+M = [1 2; 3 4] # Matrix{Int64}
+[i^2 for i in 1:3] # list comprehension a la python
 
-## [1] Simulations and Plots
+# indexing
+x[1]
+M[1,2]
+nt[1]
+nt.c
 
-# loading packages
+# [1.2] composite types (objects) 
+# with parametric type (real and imaginary part have the same numeric type)
+mutable struct ComplexNumber{T<:Real}
+  re::T 
+  im::T
+end
+
+# a constructor function is automatically added
+z = ComplexNumber(1, 0)
+
+# not ok, because real and imaginary part do not have the same type
+z = ComplexNumber(1.0, 0)
+
+# [1.3] Multiple Dispatch: A key feature of julia 
+
+# ...but we can add constructors to allow promotion to the closest super type
+function ComplexNumber(r::Real, i::Real)
+  r, i = promote(r, i)
+  return ComplexNumber(r, i)
+end
+ComplexNumber(r) = ComplexNumber(r, zero(r))
+
+# Lets add a function for pretty printing of our type to the REPL
+Base.show(io::IO, ::MIME"text/plain", z::ComplexNumber) = print(io, "$(z.re) + $(z.im)i")
+
+# now this works
+ComplexNumber(2.0, 1)
+ComplexNumber(1)
+
+# we can also define the behaviour for existing functions 
+import Base: +
++(x::ComplexNumber,y::ComplexNumber) = ComplexNumber(x.re + y.re, x.im + y.im)
+
+z1, z2 = ComplexNumber(1, 2), ComplexNumber(3.0, -1.2)
+z1 + z2 
+
+# Another illustrating example 
+abstract type Animal end
+struct Dog  <: Animal
+  name::String
+  age::Int64
+end
+struct Cat <: Animal
+  name::String
+  age::Int64
+end
+struct Mouse <: Animal
+  name::String
+  age::Int64
+end
+
+# define interactions
+meet(x::Dog, y::Cat) = println("Dog $(x.name) chases cat $(y.name) up a tree")
+meet(x::Mouse, y::Dog) = println("Mouse $(x.name) scares dog $(y.name) away")
+meet(x::Cat, y::Mouse) = println(x.age > 10 ? "Cat $(x.name) is to old to see mouse $(y.name)" : "Cat $(x.name) sneaks up on mouse $(y.name)")
+# interactions are symmetric
+meet(x::Animal, y::Animal) = meet(y, x)
+
+meet(Cat("Bob", 10), Dog("Alice", 1))
+meet(Dog("Alice", 1), Mouse("Jerry", 10))
+meet(Cat("Tom", 15), Mouse("Jerry", 10))
+meet(Cat("Bill", 8), Mouse("Jerry", 10))
+
+# the compiler automatically dispatches to the best match
+# we can check that with the macro `@which`
+@which meet(Cat("Bob", 10), Dog("Alice", 1))
+@which meet(Dog("Alice", 1), Cat("Bob", 10))
+
+## [2] ODE Simulations and Plots
+
+# loading packages (this makes all exported symbols of a package available in the Main namespace)
 using DifferentialEquations 
 using GLMakie
 
-# choose ode solver (this is a Runge-Kutta solver that switches to Rodas4P if
-# stiffness is detected)
-ode_alg = AutoTsit5(Rodas4P());
-
-# define RHS of ODE system (function must have this signature and mutates du)
+# define RHS of ODE system (function must have this signature)
+# note that f! mutates du (in julia mutating functions are typically indicated with `!`)
 function f!(du, u, p, t)
   x, y = u
   ρ, α, θ, δ, γ = p 
@@ -19,7 +97,7 @@ function f!(du, u, p, t)
     ρ*x*(1-x) - α/(θ + x)*x*y
     -δ*y + γ*α/(θ + x)*x*y
   ]
-  return nothing
+  return nothing # return `nothing`, update `du` directly (in-place function)
 end
 
 # inital conditions, parameters and time span
@@ -29,28 +107,27 @@ tspan = (0.0, 50.0)
 
 # Solve ODE problem
 problem = ODEProblem(f!, u0, tspan, p)
-sol = solve(problem, ode_alg; saveat=LinRange(tspan..., 501))
+sol = solve(problem; saveat=LinRange(tspan..., 501))
 
 # simple plot 
-lines(sol)
+fig, ax, l = lines(sol; label=["x", "y"])
+Legend(fig[1,2], l, ["x", "y"])
 
 # Alternatively: Plot with some more control 
-# plot ODESolution type with Makie as series
+# tell Makie how to plot ODESolution as series
 Makie.convert_arguments(T::Type{<:Series}, sol::ODESolution) = Makie.convert_arguments(T, sol.t, hcat(sol.u...))
 
 fig, ax, s = series(sol; labels=["x", "y"], color=[Makie.wong_colors()[1], Makie.wong_colors()[2]]);
 axislegend(ax)
 fig
 
-## Integration with other packages is generally very nice in julia
+## Integration with other packages is generally very nice in julia (duo to multiple dispatch)
 using Measurements
 
-p = (ρ=1.0 ± 0.04, α=2.0 ± 0.05, θ=1.0 ± 0.08, δ=0.5 ± 0.01, γ=1.0 ± 0.09)
-p = (ρ=2.4 ± 0.05, α=5.4 ± 0.04, θ=0.8 ± 0.08, δ=0.4 ± 0.01, γ=1.0 ± 0.09)
-p = [1.0 ± 0.05, 2.0 ± 0.1, 1.0 ± 0.08, 0.5 ± 0.01, 1.0 ± 0.09]
-# p = [1.0 ± 0.05, 2.0 ± 0.1, 1.0 ± 0.08, 0.5 ± 0.01, 1.0 ± 0.09]
-u0 = [0.1, 0.2] .± 0.0
-tspan = (0.0, 30.0) .± 0.0
+# p = (ρ=2.4 ± 0.05, α=5.4 ± 0.04, θ=0.8 ± 0.08, δ=0.4 ± 0.01, γ=1.0 ± 0.09)
+p = (ρ=1.8 ± 0.05, α=0.9 ± 0.04, θ=0.8 ± 0.08, δ=0.2 ± 0.01, γ=1.0 ± 0.09)
+u0 = [0.1, 0.2] .± 0.05
+tspan = (0.0, 100.0) .± 0.0
 problem = ODEProblem(f!, u0, tspan, p)
 sol = solve(problem; saveat=LinRange(tspan..., 501))
 
@@ -58,8 +135,70 @@ fig = Figure();
 ax = Axis(fig[1,1])
 band!(ax, sol.t, sol[1,:]; color=Makie.wong_colors()[1], alpha=0.3)
 band!(ax, sol.t, sol[2,:]; color=Makie.wong_colors()[2], alpha=0.3)
-lines!(ax, sol.t, sol[1,:]; color=Makie.wong_colors()[1])
-lines!(ax, sol.t, sol[2,:]; color=Makie.wong_colors()[2])
+lines!(ax, sol.t, sol[1,:]; color=Makie.wong_colors()[1], label="x")
+lines!(ax, sol.t, sol[2,:]; color=Makie.wong_colors()[2], label="y")
+axislegend(ax)
+fig
+
+## [3] GUI 
+
+p = (ρ=1.8, α=2.1, θ=0.4, δ=0.3, γ=0.3)
+u0 = [0.1, 0.2]
+tspan = (0.0, 100.0)
+
+fig = Figure(size=(1200,800));
+ax = Axis(fig[1,1]);
+
+# create sliders for interactivity
+slider_names = [string.([keys(p)...]); ["x(0)", "y(0)"]]
+sliders = [(
+  label = name,
+  range = 0:0.1:10,
+  format = x -> "$x"
+) 
+  for name in slider_names
+]
+# organize sliders in grid
+lsgrid = SliderGrid(fig[1:2,2], sliders..., tellheight=false)
+# sliders values as Vector of observables
+sliderobservables = [s.value for s in lsgrid.sliders]
+# slider values as observable Vector (listens to changes in any slider)
+slider_values = lift(sliderobservables...) do slvalues...
+  [slvalues...]
+end
+# set to inital values
+for i in 1:length(sliderobservables)
+  set_close_to!(lsgrid.sliders[i], [values(p)..., u0...][i])
+end
+
+# add slider for tspan 
+T_end_slider = SliderGrid(fig[2,1], (label = "T", range = [10, 100, 200, 500, 1000, 2000, 5000, 10000], format = x->"$x"))
+T_end = T_end_slider.sliders[1].value
+
+# solve ode problem with parameters defined by slider
+_p = @lift $(slider_values)[1:length(p)]
+_u0 = @lift $(slider_values)[length(p)+1:end]
+_tspan = @lift (0.0, $T_end)
+# remake and solve ODEProblem with new arguments
+prob = @lift remake(problem; u0=$_u0, tspan=$_tspan, p=$_p);
+sol = @lift solve($prob; saveat=LinRange(0.0, $T_end, 501));
+# plot solution (Makie recognizes observables and updates the plot on change)
+series!(ax, sol, labels=["x", "y"], color=Makie.wong_colors()[1:2], linewidth=2)
+axislegend(ax)
+
+# update axes range on change
+on(sol) do sol
+  y_range = [minimum(sol), maximum(sol)]
+  y_pad = 0.05*(y_range[2] - y_range[1])
+  x_pad = 0.05*(sol.t[end] - sol.t[1])
+  xlims!(ax, 0.0 - x_pad, sol.t[end] + x_pad)
+  ylims!(ax, y_range[1] - y_pad, y_range[2] + y_pad)
+end
+  
+# layout
+rowgap!(lsgrid.layout, 7)
+colsize!(fig.layout, 1, Relative(2/3))
+
 fig
 
 ## [2] Fit model to data 
@@ -146,56 +285,4 @@ for k in eachindex(p_evolve)
   sleep(0.1)
 end
 
-
-## [3] GUI 
-
-fig = Figure(size=(1200,800));
-ax = Axis(fig[1,1]);
-
-# sliders for interactivity
-slider_names = [string.([keys(p)...]); ["x(0)", "y(0)"]]
-sliders = [(
-  label = name,
-  range = 0:0.1:10,
-  format = x -> "$x"
-) 
-  for name in slider_names
-]
-lsgrid = SliderGrid(fig[1:2,2], sliders..., tellheight=false)
-sliderobservables = [s.value for s in lsgrid.sliders]
-slider_values = lift(sliderobservables...) do slvalues...
-  [slvalues...]
-end
-# set to inital values
-for i in 1:length(sliderobservables)
-  set_close_to!(lsgrid.sliders[i], [values(p)..., u0...][i])
-end
-
-# add slider for tspan 
-T_end_slider = SliderGrid(fig[2,1], (label = "T", range = [10, 100, 200, 500, 1000, 2000, 5000, 10000], format = x->"$x"))
-T_end = T_end_slider.sliders[1].value
-
-# solve ode problem depending on slider
-_p = @lift $(slider_values)[1:length(p)]
-_u0 = @lift $(slider_values)[length(p)+1:end]
-_tspan = @lift (0.0, $T_end)
-prob = @lift remake(problem; u0=$_u0, tspan=$_tspan, p=$_p);
-sol = @lift solve($prob, ode_alg; saveat=LinRange(0.0, $T_end, 501));
-series!(ax, sol, labels=["x", "y"], color=Makie.wong_colors()[1:2], linewidth=2)
-axislegend(ax)
-
-# update y axis range on change
-on(sol) do sol
-  y_range = [minimum(sol), maximum(sol)]
-  y_pad = 0.05*(y_range[2] - y_range[1])
-  x_pad = 0.05*(sol.t[end] - sol.t[1])
-  xlims!(ax, 0.0 - x_pad, sol.t[end] + x_pad)
-  ylims!(ax, y_range[1] - y_pad, y_range[2] + y_pad)
-end
-  
-# layout
-rowgap!(lsgrid.layout, 7)
-colsize!(fig.layout, 1, Relative(2/3))
-
-fig
 
